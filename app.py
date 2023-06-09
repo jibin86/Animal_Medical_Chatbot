82194d49b7a41e73a06df30055f65400924d27f9
jibin86 <sjbpsh1@sookmyung.ac.kr>
2023-06-09 20:16:52 +0900

    perf: 코드 실행 속도 개선

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ from sentence_transformers import SentenceTransformer
 
 app = Flask(__name__)
 
+data_path = 'Vat_QnA_embedding_final.csv'
 # 데이터 로드
-train_data = pd.read_csv("/content/drive/MyDrive/Vat_QnA.csv")
-embeddings = train_data["embedding"]
+train_data = pd.read_csv(data_path)
+embeddings = train_data["embedding"].apply(lambda x: np.fromstring(x[1:-1], sep=' '))
+embeddings = np.vstack(embeddings)
 # 모델 로드
 model = SentenceTransformer(
     "sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens"
@@ -28,16 +30,17 @@ def get_bot_message():
     return jsonify({"botMessage": bot_message})  # 답변을 JSON 형태로 반환
 
 
-def cos_sim(A, B):
-    return dot(A, B) / (norm(A) * norm(B))
+# def cos_sim(A, B):
+#     return dot(A, B) / (norm(A) * norm(B))
 
 
 def return_answer(question):
     embedding = model.encode(question)
-    train_data["score"] = train_data.apply(
-        lambda x: cos_sim(x["embedding"], embedding), axis=1
-    )
-    return train_data.loc[train_data["score"].idxmax()]["answer"]
+    embedding = embedding.reshape(1, -1)  # 형태 변환: (n,) -> (1, n)
+    scores = dot(embeddings, embedding.T) / (norm(embeddings, axis=1) * norm(embedding)).reshape(-1, 1)
+    max_score_index = np.argmax(scores)
+    return train_data.loc[max_score_index]["answer"]
+
 
 
 if __name__ == "__main__":
